e143c6b944c9ca7cf3cd9a9ca4365b5e61938902
midestic <82858666+midestic@users.noreply.github.com>
2023-08-09 04:13:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+console.clear();
+
+var originA = document.getElementById('box-A');
+var originS = document.getElementById('box-S');
+var originD = document.getElementById('box-D');
+var originF = document.getElementById('box-F');
+var originG = document.getElementById('box-G');
+var originH = document.getElementById('box-H');
+var originJ = document.getElementById('box-J');
+var originK = document.getElementById('box-K');
+var originL = document.getElementById('box-L');
+    
+
+
+originA.addEventListener('click', function() {
+
+    var song = new Audio('clap.wav')
+song.play();
+originA.style.boxShadow =
+         '0px 0px 20px 5px yellow'
+
+});
+
+
+
+
+
+originS.addEventListener('click', function() {
+
+    var song = new Audio('hihat.wav')
+song.play()
+
+});
+
+originD.addEventListener('click', function() {
+
+    var song = new Audio('kick.wav')
+song.play()
+
+});
+
+originF.addEventListener('click', function() {
+
+    var song = new Audio('openhat.wav')
+song.play()
+
+});
+
+originG.addEventListener('click', function() {
+
+    var song = new Audio('boom.wav')
+song.play()
+
+});
+
+originH.addEventListener('click', function() {
+
+    var song = new Audio('ride.wav')
+song.play()
+
+});
+
+originJ.addEventListener('click', function() {
+
+    var song = new Audio('snare.wav')
+song.play()
+
+});
+
+originK.addEventListener('click', function() {
+
+    var song = new Audio('tom.wav')
+song.play()
+
+});
+
+originL.addEventListener('click', function() {
+
+    var song = new Audio('tink.wav')
+song.play()
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//  originA.addEventListener('click', function() {
+//      
+
+
+//  })
+
+
+
+
